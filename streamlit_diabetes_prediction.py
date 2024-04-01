700429679e6a6846fdd4fc9e3a99c47e3a064058
IronCoder <142227723+prathameshatkare@users.noreply.github.com>
2024-04-01 14:33:45 +0530

    Update streamlit_diabetes_prediction.py

--- streamlit_diabetes_prediction.py
+++ streamlit_diabetes_prediction.py
@@ -1,52 +1,35 @@
-# importing Important Liberaries
-import pickle
 import streamlit as st
-import numpy as np
 
-# Load model
-model_diabetes = pickle.load(open('model_diabetes.sav', 'rb'))
+# Title
+st.title('Diabetic Prediction System')
 
-# Web Title
-st.title('Diabetes Prediction')
+# Sidebar
+st.sidebar.header('User Input Features')
 
-# Split Columns
-col1, col2 = st.columns(2)
+# Collecting user input features
+def user_input_features():
+    pregnancies = st.sidebar.slider('Pregnancies', 0, 17, 3)
+    glucose = st.sidebar.slider('Glucose', 0, 199, 117)
+    blood_pressure = st.sidebar.slider('Blood Pressure', 0, 122, 72)
+    skin_thickness = st.sidebar.slider('Skin Thickness', 0, 99, 23)
+    insulin = st.sidebar.slider('Insulin', 0, 846, 30)
+    bmi = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
+    dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.078, 2.42, 0.372)
+    age = st.sidebar.slider('Age', 21, 81, 29)
 
-with col1 :
-  Pregnancies = st.number_input('Enter the Pregnancies value')
+    features = {
+        'Pregnancies': pregnancies,
+        'Glucose': glucose,
+        'Blood Pressure': blood_pressure,
+        'Skin Thickness': skin_thickness,
+        'Insulin': insulin,
+        'BMI': bmi,
+        'Diabetes Pedigree Function': dpf,
+        'Age': age
+    }
+    return features
 
-with col2 :
-  Glucose = st.number_input('Enter the Glucose value')
-  
-with col1 :
-  BloodPressure = st.number_input('Enter the Blood Pressure value')
-
-with col2 :
-  SkinThickness = st.number_input('Enter the Skin Thickness value')
-
-with col1 :
-  Insulin = st.number_input('Enter the Insulin value')
-
-with col2 :
-  BMI = st.number_input('Enter the BMI value')
-
-with col1 :
-  DiabetesPedigreeFunction = st.number_input('Enter the Diabetes Pedigree Function value')
-
-with col2 :
-  Age = st.number_input('Enter the Age value')
-  
-# Prediction
-diabetes_diagnosis = ''
-
-if st.button('Diabetes Prediction Test'):
-  diabetes_prediction = model_diabetes.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
-  
-  if(diabetes_prediction[0]==1):
-    diabetes_diagnosis = 'The patient has diabetes'
-  else :
-    diabetes_diagnosis = 'The patient does not have diabetes'
- 
-
-st.success(diabetes_diagnosis)
-
+# Display user input features
+user_input = user_input_features()
+st.write('User Input Features:')
+st.write(user_input)
